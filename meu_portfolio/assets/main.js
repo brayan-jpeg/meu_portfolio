2438b936551257aabd75e668057b30bb6d1da632
brayan-jpeg <silvabritobrayan@gmail.com>
2024-12-01 21:01:50 -0300

    Add files via upload

---
+++ meu_portfolio/assets/main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.animate-fade-in', {
+    distance: '90px',   // Distância inicial
+    origin: 'top',      // Origem do movimento ('top', 'bottom', 'left', 'right')
+    duration: 1800,      // Duração da animação (em ms)
+    easing: 'ease-in-out', // Curva de animação
+    reset: false,       // Reaplicar animação ao rolar novamente
+});
+
